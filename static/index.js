32ff6ef7a91f015ad534cf38ff69ac5bfa19a58b
maytees <mateespublicprofile@gmail.com>
2024-02-11 20:07:19 -0500

    ee

--- static/index.js
+++ static/index.js
@@ -1,29 +1,48 @@
 const form = document.querySelector("form");
-const input = document.querySelector("input");
-let verified = true;
 
-// Thanks, codebubb
-const sha256script = document.createElement("script");
-sha256script.src =
-  "https://cdnjs.cloudflare.com/ajax/libs/js-sha256/0.9.0/sha256.min.js";
+const baseUrl = 'http://localhost:7070';
 
-document.head.appendChild(sha256script);
+let passEelement = document.getElementById('passphrase');
+let nameElement = document.getElementById('name');
 
-function getCookie(name) {
-  var match = document.cookie.match(RegExp("(?:^|;\\s*)" + name + "=([^;]*)"));
-  return match ? match[1] : false;
-}
+let passphrase = '';
+let user = '';
+
+passEelement.addEventListener('input', (e) => {
+  passphrase = e.target.value;
+});
+
+nameElement.addEventListener('input', (e) => {
+  user = e.target.value;
+});
+
+// Send a fetch request to vizard.matees.net/ 
+fetch(baseUrl, {
+  credentials: 'include' // Include cookies in the request,
+});
 
 form.addEventListener("submit", async (event) => {
-if(!verified) {alert("Put in the password dum dum");return};
   event.preventDefault();
   window.navigator.serviceWorker
     .register("./sw.js", {
       scope: __uv$config.prefix,
     })
     .then(() => {
+      // send passphrase over to the server via get req to baseUrl + /check, with name=user and pass=passphrase query params and store response json in a variable, the json given back is a boolean in json
+      console.log(user, passphrase);
+      fetch(baseUrl + '/check?name=' + user + '&pass=' + passphrase, {
+        credentials: 'include'
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.result) {
+          window.location.href = __uv$config.prefix + __uv$config.encodeUrl("https://outred.github.io");
+        } else {
+          alert('Sharing is not caring. if you know who owns the site, go to them and ask for it, not someone else');
+        }
+      });
 
-      window.location.href = __uv$config.prefix + __uv$config.encodeUrl("https://outred.github.io");
+      // window.location.href = __uv$config.prefix + __uv$config.encodeUrl("https://outred.github.io");
     });
 });
 
@@ -35,40 +54,3 @@ function isUrl(val = "") {
     return true;
   return false;
 }
-
-// let verified = false;
-
-// async function getPass() {
-// console.log("getPass()")
-//  // return "d979885447a413abb6d606a5d0f45c3b7809e6fde2c83f0df3426f1fc9bfed97";
-//   // return await fetch("https://dashboard.matees.net/proxy/password/");
-// }
-
-// let password_hash;
-// let pass;
-//     while (!verified || pass == null) {
-// console.log("asdf")
-//       if (getCookie("verified") == "yes") {
-//         verified = true;
-//         break;
-//       }
-
-//       pass = prompt("What is the password?");
-
-//       //let input_hash = sha256(pass);
-
-//       if (pass === "andrew") {
-//         verified = true;
-//         document.cookie = "verified=yes; expires=Tue, 19 Jan 2038 04:14:07 GMT";
-//       } else {
-//         console.log("Wrong hash");
-//       }
-//     }
-
-// getPass().then((res) => { console.log("then")
-//   res.text().then((text) => {
-// 	console.log(text);
-//     password_hash = text;
-
-//   });
-// });
